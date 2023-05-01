944f50cfa5643102bc2078037417cb8a65824898
ubedthaheem <usthaheem@yahoo.com>
2023-05-02 02:30:33 +0500

    Update Front End

--- app/src/main/java/com/ubaid/nytimesnews/DetailActivity.java
+++ app/src/main/java/com/ubaid/nytimesnews/DetailActivity.java
@@ -1,14 +1,19 @@
 package com.ubaid.nytimesnews;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 
 import com.bumptech.glide.Glide;
 import com.ubaid.nytimesnews.databinding.ActivityDetailBinding;
+
+import java.util.Objects;
 
 public class DetailActivity extends AppCompatActivity {
 
@@ -19,8 +24,10 @@ public class DetailActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         binding = ActivityDetailBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+        setSupportActionBar(binding.toolbarMain);
+        Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
 
-        binding.aboutAppBackBtn.setOnClickListener(v -> onBackPressed());
         intent = getIntent();
         if (intent !=null){
             load_views();
@@ -28,10 +35,22 @@ public class DetailActivity extends AppCompatActivity {
     }
 
 
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == android.R.id.home){
+            onBackPressed();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
     private void load_views(){
         Glide.with(this).load(intent.getStringExtra("thumbnail")).into(binding.imageView);
         binding.newsDetail.setText(getText("excerpt"));
         binding.newsTitle.setText(getText("title"));
+        binding.toolbarMain.setTitle(getText("title"));
+        binding.collapsingToolbar.setExpandedTitleColor(ContextCompat.getColor(this, android.R.color.transparent));
+        binding.collapsingToolbar.setTitle(getText("title"));
+
 
         binding.newsButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -44,6 +63,11 @@ public class DetailActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+    @Override
+    public void onBackPressed() {
+        this.finish();
     }
 
     private String getText(String from){
